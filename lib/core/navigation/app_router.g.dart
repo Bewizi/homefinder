bb2ac77490912863ce2eb6d99de6cf17c3d58cc1
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-17 13:10:40 +0100

    refactor: Update app routing to introduce a splash screen route
    This commit refactors the application's routing configuration to set the splash screen as the initial route.
    
    - The root path (`/`) now directs to a new `SplashScreenRoute`.
    - The `GetStartedRoute` is now a sub-route of the splash screen, accessible at the `/get-started` path.
    - The generated `go_router` code has been updated to reflect these changes, ensuring type-safe navigation remains consistent with the new route structure.

--- lib/core/navigation/app_router.g.dart
+++ lib/core/navigation/app_router.g.dart
@@ -6,16 +6,45 @@ part of 'app_router.dart';
 // GoRouterGenerator
 // **************************************************************************
 
-List<RouteBase> get $appRoutes => [$getStartedRoute];
+List<RouteBase> get $appRoutes => [$splashScreenRoute];
 
-RouteBase get $getStartedRoute =>
-    GoRouteData.$route(path: '/', factory: $GetStartedRoute._fromState);
+RouteBase get $splashScreenRoute => GoRouteData.$route(
+  path: '/',
+  factory: $SplashScreenRoute._fromState,
+  routes: [
+    GoRouteData.$route(
+      path: 'get-started',
+      factory: $GetStartedRoute._fromState,
+    ),
+  ],
+);
+
+mixin $SplashScreenRoute on GoRouteData {
+  static SplashScreenRoute _fromState(GoRouterState state) =>
+      SplashScreenRoute();
+
+  @override
+  String get location => GoRouteData.$location('/');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
 
 mixin $GetStartedRoute on GoRouteData {
   static GetStartedRoute _fromState(GoRouterState state) => GetStartedRoute();
 
   @override
-  String get location => GoRouteData.$location('/');
+  String get location => GoRouteData.$location('/get-started');
 
   @override
   void go(BuildContext context) => context.go(location);
