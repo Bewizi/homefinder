db3f5dca885280a7434151f427c1f91c995f36e6
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-17 20:44:51 +0100

    feat: Add amico image assets
    This commit adds three new image assets to the `AppImages` class.
    
    - The new constants `kAmico`, `kAmico2`, and `kAmico3` all point to the same image path: `assets/images/amico.png`.

--- lib/core/variables/app_images.dart
+++ lib/core/variables/app_images.dart
@@ -2,4 +2,8 @@ class AppImages {
   static const String kGetStartedBg = 'assets/images/image_fx.png';
 
   static const String kLogoWhite = 'assets/images/mdi_home-search.png';
+
+  static const String kAmico = 'assets/images/amico.png';
+  static const String kAmico2 = 'assets/images/amico.png';
+  static const String kAmico3 = 'assets/images/amico.png';
 }
