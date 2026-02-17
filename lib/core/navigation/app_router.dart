e2194bdc6d0ea071bbf836db15afbf3b82647225
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-17 13:10:25 +0100

    feat: Update routing to introduce splash screen
    This commit modifies the application's navigation to set the splash screen as the initial route.
    
    - The `initialLocation` in `GoRouter` is changed from the get started page to the new splash screen path.
    - A new `SplashScreenRoute` is defined with the path `/`.
    - The `GetStartedRoute` path is updated to `/get-started`.

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:homefinder/features/getStarted/get_started.dart';
+import 'package:homefinder/features/splash_screen/splash_screen.dart';
 
 part 'app_router.g.dart';
 
 final appRouter = GoRouter(
   routes: $appRoutes,
-  initialLocation: GetStartedRoute.path,
+  initialLocation: SplashScreenRoute.path,
 );
 
-@TypedGoRoute<GetStartedRoute>(path: '/')
-class GetStartedRoute extends GoRouteData with $GetStartedRoute {
+@TypedGoRoute<SplashScreenRoute>(path: SplashScreenRoute.path)
+class SplashScreenRoute extends GoRouteData with $SplashScreenRoute {
   static const path = '/';
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const SplashScreen();
+}
+
+@TypedGoRoute<GetStartedRoute>(path: GetStartedRoute.path)
+class GetStartedRoute extends GoRouteData with $GetStartedRoute {
+  static const path = '/get-started';
 
   @override
   Widget build(BuildContext context, GoRouterState state) => const GetStarted();
