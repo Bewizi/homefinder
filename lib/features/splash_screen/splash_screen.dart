3526c9479eb54cfad369d6e77b8bcbf7b480be47
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-17 13:11:20 +0100

    feat: Enhance splash screen with logo and timed navigation
    This commit converts the `SplashScreen` from a `StatelessWidget` to a `StatefulWidget` to introduce timed navigation.
    
    - The screen now displays the application logo and name.
    - After a 5-second delay, the user is automatically navigated to the `GetStartedRoute`.
    - The primary color is used for the background.

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:homefinder/core/navigation/app_router.dart';
+import 'package:homefinder/core/ui/components/app_text.dart';
 import 'package:homefinder/core/ui/components/layouts/app_scaffold.dart';
+import 'package:homefinder/core/variables/app_images.dart';
 import 'package:homefinder/core/variables/colors.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  static const String routeName = '/splashScreen';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        if (mounted) {
+          GetStartedRoute().go(context);
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AppScaffold(backgroundColor: AppColors.kPrimary, body: Center());
+    return AppScaffold(
+      backgroundColor: AppColors.kPrimary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppImages.kLogoWhite,
+              width: 100,
+              height: 100,
+            ),
+            AppText(
+              'Homefinder',
+              style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
