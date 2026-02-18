846a9876ef732449c81269603c6c27e7b75ec6fe
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-19 00:34:08 +0100

    feat: Add reusable text field component
    This commit introduces a new reusable `AppTextField` widget.
    
    - **`AppTextField`**: A `StatelessWidget` that wraps a `TextFormField` with consistent styling.
    - **Features**:
        - Optional `title` displayed above the text field.
        - Customizable `hintText`, `validator`, `keyboardType`, and `obscureText` for password fields.
        - Consistent border radius and padding are applied.

---
+++ lib/core/ui/components/app_text_field.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:homefinder/core/ui/components/app_text.dart';
+import 'package:homefinder/core/ui/extensions/app_spacing_extension.dart';
+import 'package:homefinder/core/variables/app_radius.dart';
+import 'package:homefinder/core/variables/colors.dart';
+
+class AppTextField extends StatelessWidget {
+  const AppTextField({
+    this.controller,
+    this.hintText,
+    this.showTitle = true,
+    this.title,
+    this.validator,
+    this.keyboardType,
+    this.obscureText = false,
+    super.key,
+  });
+
+  final TextEditingController? controller;
+  final String? hintText;
+  final bool showTitle;
+  final String? title;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  Widget _buildTextFormField(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppRadius.medium),
+          borderSide: const BorderSide(color: AppColors.kGrey5),
+        ),
+        hintText: hintText,
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 12,
+        ),
+        hintStyle:
+            Theme.of(
+              context,
+            ).textTheme.bodySmall?.copyWith(
+              color: AppColors.kGray30,
+            ),
+      ),
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (showTitle && title != null) ...[
+          AppText(
+            title!,
+            style:
+                Theme.of(
+                  context,
+                ).textTheme.titleSmall?.copyWith(
+                  color: AppColors.kGray50,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+        ],
+        8.verticalSpacing,
+        _buildTextFormField(context),
+      ],
+    );
+  }
+}
