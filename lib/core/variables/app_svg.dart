84d4e0c6e985271d4976e3f0380045b8b7ce4bf0
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-19 16:57:09 +0100

    feat: Add navigation bar icon assets
    This commit adds new SVG asset paths to the `AppSvg` constants class for use in the navigation bar.
    
    - New constants `kSaved`, `kProfile`, `kMessages`, and `kExplore` have been added, pointing to their respective SVG files.

--- lib/core/variables/app_svg.dart
+++ lib/core/variables/app_svg.dart
@@ -1,3 +1,9 @@
 class AppSvg {
   static const String kLogoBlue = 'assets/svgs/mdi_home-search.svg';
+
+  //   navigation bar icon
+  static const String kSaved = 'assets/svgs/heart.svg';
+  static const String kProfile = 'assets/svgs/profile-circle.svg';
+  static const String kMessages = 'assets/svgs/sms.svg';
+  static const String kExplore = 'assets/svgs/search-normal.svg';
 }
