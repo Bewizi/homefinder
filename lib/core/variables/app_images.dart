94249ac9674ee010f2eeb330b58abba61895151b
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-20 05:05:45 +0100

    feat: Add new image asset constants
    This commit adds new image asset paths to the `AppImages` class.
    
    - New constants `kAmico`, `kAmico2`, and `kAmico3` are added for onboarding images.
    - A new constant `kStarlightResidence` is added for home-related images.
    - Comments have been added to group the image constants by their purpose (`onboarding images` and `home near me images`).

--- lib/core/variables/app_images.dart
+++ lib/core/variables/app_images.dart
@@ -3,7 +3,12 @@ class AppImages {
 
   static const String kLogoWhite = 'assets/images/mdi_home-search.png';
 
+  // onboarding images
   static const String kAmico = 'assets/images/amico.png';
   static const String kAmico2 = 'assets/images/amico 2.png';
   static const String kAmico3 = 'assets/images/amico 3.png';
+
+  //   home near me images
+  static const String kStarlightResidence =
+      'assets/images/Starlight Residence.png';
 }
