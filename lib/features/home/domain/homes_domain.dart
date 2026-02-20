d40d74efe0a056cf64bab7d7742602af74bac1d2
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-20 05:06:29 +0100

    feat: Add HomesDomain data model
    This commit introduces a new data model, `HomesDomain`, for the home feature.
    
    - **`HomesDomain` class**: A new file `lib/features/home/domain/homes_domain.dart` is created.
    - **Properties**: The class includes final properties for `id`, `name`, `images`, and `rating`.

---
+++ lib/features/home/domain/homes_domain.dart
@@ -0,0 +1,13 @@
+class HomesDomain {
+  HomesDomain(
+    this.id,
+    this.name,
+    this.images,
+    this.rating,
+  );
+
+  final String id;
+  final String name;
+  final String images;
+  final String rating;
+}
