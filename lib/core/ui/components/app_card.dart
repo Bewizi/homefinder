d454f5b91cf1819fad48e1188f9744c4e8c36dea
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-25 21:44:19 +0100

    refactor: Update `AppCard` component styling
    This commit updates the visual appearance of the `AppCard` by adding a border and adjusting its shadow.
    
    - **`AppCard`**:
        - Added a border using `AppColors.kGrey5`.
        - Updated `BoxShadow` offset from `(0, 1)` to `(0, 2)`.
        - Simplified the alpha value in `withValues` from `0.10` to `0.1`.

--- lib/core/ui/components/app_card.dart
+++ lib/core/ui/components/app_card.dart
@@ -14,10 +14,11 @@ class AppCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppColors.kWhite,
         borderRadius: BorderRadius.circular(AppRadius.lager),
+        border: Border.all(color: AppColors.kGrey5),
         boxShadow: [
           BoxShadow(
-            color: AppColors.kGrey80.withValues(alpha: 0.10),
-            offset: const Offset(0, 1),
+            color: AppColors.kGrey80.withValues(alpha: 0.1),
+            offset: const Offset(0, 2),
             blurRadius: 3,
           ),
         ],
