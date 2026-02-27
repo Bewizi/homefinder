d2734e9beb6254f52921daaa9a61d1e6964cb84d
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-02-27 19:36:48 +0100

    refactor: Restrict `DevicePreview` to desktop platforms in development
    This commit updates the `main_development.dart` configuration to ensure `DevicePreview` is only enabled when running in non-release mode on Windows, macOS, or Linux.
    
    - **`main_development.dart`**:
        - Added imports for `dart:io` and `package:flutter/foundation.dart`.
        - Modified `DevicePreview` initialization to include an `enabled` condition checking for `!kReleaseMode` and desktop platforms.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:homefinder/app/app.dart';
 import 'package:homefinder/bootstrap.dart';
 
 Future<void> main() async {
-  await bootstrap(() => DevicePreview(builder: (context) => const App()));
+  await bootstrap(
+    () => DevicePreview(
+      enabled:
+          !kReleaseMode &&
+          (Platform.isWindows || Platform.isMacOS || Platform.isLinux),
+      builder: (context) => const App(),
+    ),
+  );
 }
