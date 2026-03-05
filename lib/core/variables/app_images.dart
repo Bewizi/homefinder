3a89b9289205daaac78d3f880abe7cf54d481c97
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-05 23:48:47 +0100

    feat: Add image asset constants for `Starlight Residence`
    This commit introduces several new image asset constants to `AppImages` representing different rooms and areas within the Starlight Residence property.
    
    - **`AppImages`**:
        - Added `kStarlightResidenceLivingRoom`.
        - Added `kStarlightResidenceDinningRoom`.
        - Added `kStarlightResidenceMastersBedroom`.
        - Added `kStarlightResidenceGuestRoom`.
        - Added `kStarlightResidenceClothest`.
        - Added `kStarlightResidenceKitchen`.
        - Added `kStarlightResidenceBathRoom`.

--- lib/core/variables/app_images.dart
+++ lib/core/variables/app_images.dart
@@ -12,6 +12,22 @@ class AppImages {
   static const String kStarlightResidence =
       'assets/images/Starlight Residence.png';
 
+  // starlight residence images
+  static const String kStarlightResidenceLivingRoom =
+      'assets/images/949bcc4d8dafbdf1bb3441c294b1f85400faddf9.png';
+  static const String kStarlightResidenceDinningRoom =
+      'assets/images/66f3458661b10e6333f57908c4db4d1ad66dddbe.png';
+  static const String kStarlightResidenceMastersBedroom =
+      'assets/images/0a33881ccacb8ad1aa8529129b3778e06eb60998.png';
+  static const String kStarlightResidenceGuestRoom =
+      'assets/images/8dfeb5d02d29a7fd0c18c7ba98048ee9ce100777.png';
+  static const String kStarlightResidenceClothest =
+      'assets/images/50d275dd5d89c7384023aa7012483b2c92c239c8.png';
+  static const String kStarlightResidenceKitchen =
+      'assets/images/9cbac1f87d73152ce826f87dfbebd04084568592.png';
+  static const String kStarlightResidenceBathRoom =
+      'assets/images/602d70e3a3d7710e23fb195dd5a20cc3513d7582.png';
+
   //   users
   static const String kEleanorPena = 'assets/images/Eleanor Pena.jpg';
 }
