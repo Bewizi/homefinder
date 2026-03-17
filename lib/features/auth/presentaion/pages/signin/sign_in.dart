8aea3e126a2e6d814d6ada56cc8c127a183f78bf
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-17 14:17:05 +0100

    feat: Integrate `AuthBloc` and form validation in `SignIn` page
    This commit integrates the `AuthBloc` to handle authentication states and adds form validation logic to the `SignIn` page.
    
    - **`SignIn`**:
        - Wrapped the form with `BlocConsumer<AuthBloc, AuthState>` to handle navigation on success and display error messages via `SnackBar`.
        - Added `formKey` for global form state management and controllers for email and password fields.
        - Implemented `dispose` method to properly clean up text controllers.
        - Added validation logic for the email (required, format check) and password (required, minimum length) fields.
        - Updated `PrimaryButton` to reflect loading states and trigger the `Login` event with trimmed input values upon successful validation.
        - Ensured consistent styling and interaction for the password visibility toggle and navigation links.

--- lib/features/auth/presentaion/pages/signin/sign_in.dart
+++ lib/features/auth/presentaion/pages/signin/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
@@ -12,6 +13,7 @@ import 'package:homefinder/core/ui/extensions/app_spacing_extension.dart';
 import 'package:homefinder/core/variables/app_iconsize.dart';
 import 'package:homefinder/core/variables/app_svg.dart';
 import 'package:homefinder/core/variables/colors.dart';
+import 'package:homefinder/features/auth/presentaion/auth_bloc/auth_bloc.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -23,7 +25,17 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   bool isPasswordVisible = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,65 +75,154 @@ class _SignInState extends State<SignIn> {
           24.verticalSpacing,
 
           //   form
-          Expanded(
-            child: SingleChildScrollView(
-              child: Form(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //  email address
-                    const AppTextField(
-                      title: 'Email Address',
-                      hintText: 'Example@gmail.com',
-                      keyboardType: TextInputType.emailAddress,
-                      prefixIcon: Icon(
-                        FontAwesomeIcons.envelope,
-                        size: AppIconSize.regular,
-                      ),
-                    ),
-                    16.verticalSpacing,
+          BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is Authenticated) {
+                HomeRoute().go(context);
+              }
 
-                    //  password
-                    Column(
+              if (state is AuthError) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: AppText(state.message),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }
+            },
+            builder: (context, state) {
+              return Expanded(
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        //  email address
                         AppTextField(
-                          title: 'Password',
-                          hintText: '*****************',
-                          keyboardType: TextInputType.visiblePassword,
+                          controller: _emailController,
+                          title: 'Email Address',
+                          hintText: 'Example@gmail.com',
+                          keyboardType: TextInputType.emailAddress,
                           prefixIcon: const Icon(
-                            FontAwesomeIcons.lock,
+                            FontAwesomeIcons.envelope,
                             size: AppIconSize.regular,
                           ),
-                          obscureText: !isPasswordVisible,
-                          onSuffixIconTap: () {
-                            setState(() {
-                              isPasswordVisible = !isPasswordVisible;
-                            });
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email address';
+                            }
+                            if (!value.contains('@')) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
                           },
-                          suffixIcon: Icon(
-                            isPasswordVisible
-                                ? FontAwesomeIcons.eyeSlash
-                                : FontAwesomeIcons.eye,
-                            size: AppIconSize.regular,
-                          ),
                         ),
+                        16.verticalSpacing,
 
-                        8.verticalSpacing,
+                        //  password
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppTextField(
+                              controller: _passwordController,
+                              title: 'Password',
+                              hintText: '*****************',
+                              keyboardType: TextInputType.visiblePassword,
+                              prefixIcon: const Icon(
+                                FontAwesomeIcons.lock,
+                                size: AppIconSize.regular,
+                              ),
+                              obscureText: !isPasswordVisible,
+                              onSuffixIconTap: () {
+                                setState(() {
+                                  isPasswordVisible = !isPasswordVisible;
+                                });
+                              },
+                              suffixIcon: Icon(
+                                isPasswordVisible
+                                    ? FontAwesomeIcons.eyeSlash
+                                    : FontAwesomeIcons.eye,
+                                size: AppIconSize.regular,
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your password';
+                                }
+                                if (value.length < 6) {
+                                  return 'Password must be at least 6 characters';
+                                }
+                                return null;
+                              },
+                            ),
 
-                        //   forgot password
-                        Align(
-                          alignment: Alignment.bottomRight,
+                            8.verticalSpacing,
+
+                            //   forgot password
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: AppRichText(
+                                textAlign: TextAlign.end,
+                                spans: [
+                                  TextSpan(
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () => context.push(
+                                        ForgotPasswordRoute.path,
+                                      ),
+                                    text: 'Forgot password?',
+                                    style: Theme.of(context).textTheme.bodySmall
+                                        ?.copyWith(
+                                          color: AppColors.kPrimary,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        24.verticalSpacing,
+
+                        PrimaryButton(
+                          state is AuthLoading ? 'Signing In...' : 'Sign In',
+                          loading: state is AuthLoading,
+                          pressed: state is AuthLoading
+                              ? null
+                              : () {
+                                  if (formKey.currentState!.validate()) {
+                                    context.read<AuthBloc>().add(
+                                      Login(
+                                        email: _emailController.text.trim(),
+                                        password: _passwordController.text
+                                            .trim(),
+                                      ),
+                                    );
+                                  }
+                                },
+                        ),
+                        16.verticalSpacing,
+                        Center(
                           child: AppRichText(
-                            textAlign: TextAlign.end,
+                            textAlign: TextAlign.center,
                             spans: [
                               TextSpan(
+                                text: 'Don’t have an account? ',
+                                style:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodySmall?.copyWith(
+                                      color: AppColors.kGrey70,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                              ),
+                              TextSpan(
                                 recognizer: TapGestureRecognizer()
-                                  ..onTap = () =>
-                                      context.push(ForgotPasswordRoute.path),
-                                text: 'Forgot password?',
-                                style: Theme.of(context).textTheme.bodySmall
-                                    ?.copyWith(
+                                  ..onTap = () => SignUpRoute().go(context),
+                                text: 'Sign Up',
+                                style:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodySmall?.copyWith(
                                       color: AppColors.kPrimary,
                                       fontWeight: FontWeight.w600,
                                     ),
@@ -131,46 +232,10 @@ class _SignInState extends State<SignIn> {
                         ),
                       ],
                     ),
-                    24.verticalSpacing,
-
-                    PrimaryButton(
-                      'Sign In',
-                      pressed: () => HomeRoute().go(context),
-                    ),
-                    16.verticalSpacing,
-                    Center(
-                      child: AppRichText(
-                        textAlign: TextAlign.center,
-                        spans: [
-                          TextSpan(
-                            text: 'Don’t have an account? ',
-                            style:
-                                Theme.of(
-                                  context,
-                                ).textTheme.bodySmall?.copyWith(
-                                  color: AppColors.kGrey70,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                          ),
-                          TextSpan(
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () => SignUpRoute().go(context),
-                            text: 'Sign Up',
-                            style:
-                                Theme.of(
-                                  context,
-                                ).textTheme.bodySmall?.copyWith(
-                                  color: AppColors.kPrimary,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ],
       ),
