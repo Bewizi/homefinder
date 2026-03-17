a0a5a6a862de2825aef7f583fb3750701f934f03
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-17 13:14:53 +0100

    feat: Register `AuthBloc` in `appBlocProvider`
    This commit adds the `AuthBloc` to the global Bloc provider list, enabling authentication management throughout the application and initiating an automatic authentication status check on startup.
    
    - **`appBlocProvider`**:
        - Added `AuthBloc` to the list of global providers.
        - Initialized `AuthBloc` using `AuthRepositoryImpl`.
        - Configured the bloc to trigger the `CheckAuthStatus` event upon creation.
        - Added necessary imports for authentication data and BLoC components.

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:homefinder/features/auth/data/auth_data.dart';
+import 'package:homefinder/features/auth/presentaion/auth_bloc/auth_bloc.dart';
 import 'package:homefinder/features/home/data/homes_data.dart';
 import 'package:homefinder/features/home/data/recommended_homes_data.dart';
 import 'package:homefinder/features/home/presentation/homes_bloc/homes_bloc.dart';
 import 'package:homefinder/features/home/presentation/recommended_homes_bloc/recommended_homes_bloc.dart';
 
 final List<BlocProvider> appBlocProvider = [
+  BlocProvider<AuthBloc>(
+    create: (context) => AuthBloc(AuthRepositoryImpl())..add(CheckAuthStatus()),
+  ),
   BlocProvider<HomesBloc>(
     create: (context) => HomesBloc(
       homesRepository: HomesData(),
