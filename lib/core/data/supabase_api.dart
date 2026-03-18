fa3fe187b2d05593f61954f51d1ace640c2a188c
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-18 17:06:45 +0100

    chore: Update Supabase anonymous key
    This commit updates the `anonKey` in the `SupaBaseApi` configuration to a new JWT-based credential.
    
    - **`SupaBaseApi`**:
        - Replaced the previous `anonKey` with a valid Supabase anonymous service key.

--- lib/core/data/supabase_api.dart
+++ lib/core/data/supabase_api.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupaBaseApi {
   static const String url = 'https://xoyivdjzatxyibxxongv.supabase.co';
   static const String anonKey =
-      'sb_publishable_JOa7YStwUDhhQbYowvB4tg_Q-AKOMcn';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhveWl2ZGp6YXR4eWlieHhvbmd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2ODg3ODgsImV4cCI6MjA4OTI2NDc4OH0.COXLN1rrsJRR84wjIk_0wZcjdMjCgAhDnQ-ADXwrSzM';
 }
 
 final SupabaseClient supaBase = Supabase.instance.client;
