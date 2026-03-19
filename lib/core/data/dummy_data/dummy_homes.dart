f65f5516ebca61929104f6b7512a1fdb3c8f886e
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-19 11:59:10 +0100

    feat: Add `dummyHome` to dummy data
    This commit introduces a `dummyHome` object to provide a placeholder or loading state for home-related data within the application.
    
    - **`dummy_homes.dart`**:
        - Created a new file containing a `HomesDomain` instance with default/placeholder values for ID, name, location, and metrics.
        - Set default values for `rating`, `price_per_month`, `beds`, `baths`, and `sqft` to 0.

---
+++ lib/core/data/dummy_data/dummy_homes.dart
@@ -0,0 +1,16 @@
+import 'package:homefinder/features/home/domain/homes_domain.dart';
+
+final dummyHome = HomesDomain(
+  id: 'Loading',
+  name: 'Loading Home Name',
+  image: '',
+  rating: 0.0,
+  location: 'Loading Location ...',
+  price_per_month: 0,
+  type: 'House',
+  beds: 0,
+  baths: 0,
+  sqft: 0,
+  images: [],
+  description: '',
+);
